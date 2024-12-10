82e4bfa394822ad4ad226445613e04acbbf4cd9e
Stijn Jakobs <164928935+stijnnjakobs@users.noreply.github.com>
2024-12-10 18:04:19 +0100

    service view changed

--- themes/default/views/clients/services/index.blade.php
+++ themes/default/views/clients/services/index.blade.php
@@ -1,13 +1,18 @@
 <x-app-layout title="Services" clients>
-    <div class="nk-content nk-content-lg nk-content-fluid">
+    <div class="nk-content nk-content-fluid">
         <div class="container-xl wide-lg">
             <div class="nk-content-inner">
                 <div class="nk-content-body">
-                    <div class="nk-block-head">
-                        <div class="nk-block-head-content">
-                            <h3 class="nk-block-title page-title">{{ __('My Services') }}</h3>
-                            <div class="nk-block-des text-soft">
-                                <p>{{ __('Manage your active services and subscriptions') }}</p>
+                    <x-success class="mt-4" />
+
+                    <!-- Header Section -->
+                    <div class="nk-block-head nk-block-head-sm">
+                        <div class="nk-block-between g-3">
+                            <div class="nk-block-head-content">
+                                <h3 class="nk-block-title page-title">{{ __('My Services') }}</h3>
+                                <div class="nk-block-des text-soft">
+                                    <p>{{ __('Manage your active services and subscriptions') }}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
